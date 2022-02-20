6070eb5a79becd1b3f1fc64efb1c118d87ad56d6
Achtuur <arthurdegroot88@gmail.com>
2022-02-20 21:14:39 +0100

    updated .txt files

--- SLiCAPconfig.py
+++ SLiCAPconfig.py
@@ -32,4 +32,4 @@ IMGPATH     = 'img/'    # path for image files
 PROJECT    = 'Active antenna assignment'
 AUTHOR     = 'Arthur'
 CREATED    = '2022-02-17 17:18:09.501572'
-LASTUPDATE = '2022-02-20 15:34:57.104350'
+LASTUPDATE = '2022-02-20 21:14:17.554629'
